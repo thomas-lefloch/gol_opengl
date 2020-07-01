0a3ab6bd7c42f10b393033d399e9708e032e42c5
Le Floch Thomas <t.lefloch53@gmail.com>
2020-07-01 13:11:46 +0200

    game of life working

--- src/main.cpp
+++ src/main.cpp
@@ -23,12 +23,15 @@ constexpr int squares_per_column = (window_height - square_gutter) / (square_sid
 constexpr int grid_offset_x = window_width - squares_per_line * (square_side + square_gutter);
 constexpr int grid_offset_y = window_height - squares_per_column * (square_side + square_gutter);
 
-bool cells[squares_per_line][squares_per_column];
+int cells[squares_per_line][squares_per_column];
 
-constexpr GLfloat white[] = {1, 1, 1, 1};
-constexpr GLfloat black[] = {0, 0, 0, 0};
-constexpr GLfloat grey[] = {.5, .5, .5, .5};
+bool should_update = false;
 
+constexpr GLfloat white[] = {1.0f, 1.0f, 1.0f, 1.0f};
+constexpr GLfloat black[] = {0.0f, 0.0f, 0.0f, 0.0f};
+constexpr GLfloat grey[] = {.5f, .5f, .5f, 0.8f};
+
+// TODO: support window resizing
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) { glViewport(0, 0, width, height); }
 static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos) {
   cursor_x = xpos;
@@ -41,6 +44,7 @@ void find_corresponding_cell(double x, double y, int* cell_row, int* cell_col) {
   *cell_col = floor((y - grid_offset_y * .5) / (double)(square_side + square_gutter));
 }
 
+// TODO: support mouse hold
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
   if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
     int hovered_row, hovered_col;
@@ -50,6 +54,61 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
     else
       cells[hovered_row][hovered_col] = 1;
   }
+}
+
+void space_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+  if (key == GLFW_KEY_SPACE && action == GLFW_PRESS) should_update = !should_update;
+}
+
+void update_cells(int cells[squares_per_line][squares_per_column]) {
+  int new_cells[squares_per_line][squares_per_column];
+  std::copy(&cells[0][0], &cells[0][0] + squares_per_line * squares_per_column, &new_cells[0][0]);
+
+  for (int row = 0; row < squares_per_line; row++) {
+    for (int col = 0; col < squares_per_column; col++) {
+      int neighbors = 0;
+      // TODO: refactor neighbors counting
+      if (row > 0 && col > 0) {
+        neighbors += cells[row - 1][col - 1];
+        neighbors += cells[row - 1][col];
+        neighbors += cells[row][col - 1];
+        if (row < squares_per_line - 1 && col < squares_per_column - 1) {
+          neighbors += cells[row + 1][col + 1];
+          neighbors += cells[row + 1][col];
+          neighbors += cells[row][col + 1];
+          neighbors += cells[row + 1][col - 1];
+          neighbors += cells[row - 1][col + 1];
+        }
+      } else if (row > 0) {  // col = 0
+        neighbors += cells[row - 1][col + 1];
+        neighbors += cells[row - 1][col];
+        neighbors += cells[row][col + 1];
+        if (row < squares_per_line - 1) {
+          neighbors += cells[row + 1][col + 1];
+          neighbors += cells[row + 1][col];
+        }
+      } else if (col > 0) {  // row = 0
+        neighbors += cells[row][col - 1];
+        neighbors += cells[row + 1][col - 1];
+        neighbors += cells[row + 1][col];
+        if (col < squares_per_column - 1) {
+          neighbors += cells[row][col + 1];
+          neighbors += cells[row + 1][col + 1];
+        }
+      } else {  // row = 0 && row = 0
+        neighbors += cells[row + 1][col];
+        neighbors += cells[row][col + 1];
+        neighbors += cells[row + 1][col + 1];
+      }
+
+      if (neighbors == 3) {
+        new_cells[row][col] = 1;
+      } else if (neighbors != 2) {
+        new_cells[row][col] = 0;
+      }
+    }
+  }
+  std::copy(&new_cells[0][0], &new_cells[0][0] + squares_per_line * squares_per_column, &cells[0][0]);
 }
 
 int main() {
@@ -77,21 +136,16 @@ int main() {
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
   glfwSetCursorPosCallback(window, cursor_position_callback);
   glfwSetMouseButtonCallback(window, mouse_button_callback);
+  glfwSetKeyCallback(window, space_callback);
 
   float vertices[] = {
-      -(square_side * .5) / (window_width * .5), (square_side * .5) / (window_height * .5),  .0,  // top left
-      (square_side * .5) / (window_width * .5),  (square_side * .5) / (window_height * .5),  .0,  // top right
-      -(square_side * .5) / (window_width * .5), -(square_side * .5) / (window_height * .5), .0,  // bottom left
-      (square_side * .5) / (window_width * .5),  -(square_side * .5) / (window_height * .5), .0,  // bottom right
+      -(square_side * .5f) / (window_width * .5f), (square_side * .5f) / (window_height * .5f),  .0f,  // top left
+      (square_side * .5f) / (window_width * .5f),  (square_side * .5f) / (window_height * .5f),  .0f,  // top right
+      -(square_side * .5f) / (window_width * .5f), -(square_side * .5f) / (window_height * .5f), .0f,  // bottom left
+      (square_side * .5f) / (window_width * .5f),  -(square_side * .5f) / (window_height * .5f), .0f,  // bottom right
   };
 
   unsigned int indices[] = {0, 1, 2, 1, 2, 3};
-
-  for (int row = 0; row < squares_per_line; row++) {
-    for (int col = 0; col < squares_per_column; col++) {
-      cells[row][col] = 0;
-    }
-  }
 
   unsigned int VAO;
   glGenVertexArrays(1, &VAO);
@@ -113,10 +167,16 @@ int main() {
   int shader_id = create_shader_program(ROOT_DIR "shaders/vertex.vs", ROOT_DIR "shaders/fragment.fs");
   if (shader_id == -1) std::cout << "Error while parsing/compiling shaders" << std::endl;
 
+  // TODO limit fps
   while (!glfwWindowShouldClose(window)) {
+    int grid[squares_per_line][squares_per_column];
     glfwSwapBuffers(window);
     glfwPollEvents();
 
+    // TODO: should display indication that game is stopped
+    if (should_update) update_cells(cells);
+
+    // render
     glUseProgram(shader_id);
     glBindVertexArray(VAO);
 
